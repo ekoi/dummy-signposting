1785e9ae2dd82db8b318e4c8ec50c49c2ef81027
ekoi <eko.indarto@dans.knaw.nl>
2023-02-17 16:13:04 +0100

    Intermediate commit: add HEAD

--- src/main.py
+++ src/main.py
@@ -3,12 +3,14 @@ import logging
 import os
 
 import uvicorn
-from fastapi import FastAPI, Request, HTTPException, Depends, status
+from fastapi import FastAPI, Request
 from dynaconf import Dynaconf
 
 import importlib.metadata
 
 __version__ = importlib.metadata.metadata("dummy-signposting-service")["version"]
+
+from starlette.responses import Response
 
 settings = Dynaconf(settings_files=["conf/settings.toml"],
                     environments=True)
@@ -18,20 +20,31 @@ logging.basicConfig(filename=settings.LOG_FILE, level=settings.LOG_LEVEL,
 app = FastAPI(title=settings.FASTAPI_TITLE, description=settings.FASTAPI_DESCRIPTION,
               version=__version__)
 
+
 @app.get('/')
 def info():
     logging.info("Dummy Signposting Generator")
     logging.debug("info")
     return {"name": "Dummy Signposting Generator", "version": __version__}
 
-@app.get('/links/{json_filename}')
-def get_links(json_filename:str):
-    with open(os.path.join(settings.RESOURCES_PATH, json_filename)) as f:
+
+@app.get('/linkset/{number}/json')
+def get_links(number: str):
+    with open(os.path.join(settings.RESOURCES_PATH, f'{number}.json')) as f:
         data = json.load(f)
     return data
+
+
+@app.api_route('/page/{number}', methods=['GET', 'HEAD'])
+def get_page(number: int, response: Response, request: Request):
+    base_url = request.base_url
+    response.headers['X-Powered-By'] = 'https://github.com/ekoi/dummy-signposting'
+    response.headers['Allow'] = "GET, HEAD"
+    response.headers['Link'] = f'<{base_url}linkset/{number}/json> ; rel="linkset" ; type="application/linkset+json"'
+
+    return {"eko": number}
+
 
 if __name__ == "__main__":
     logging.info("Start")
     uvicorn.run("src.main:app", host="0.0.0.0", port=2907, reload=False)
-
-
